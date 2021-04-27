4114381724c563d06aecded70cd99cd236c56156
lchua2314 <lchua2314@gmail.com>
2021-04-26 21:42:59 -0700

    Added basic detail activity

--- app/src/main/java/com/onemanarmy/android/gamenewsapp/DetailActivity.java
+++ app/src/main/java/com/onemanarmy/android/gamenewsapp/DetailActivity.java
@@ -1,15 +1,52 @@
 package com.onemanarmy.android.gamenewsapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.onemanarmy.android.gamenewsapp.models.Game;
+
+import org.parceler.Parcels;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView tvTitle, tvDeck, tvAuthors, tvPublishDate, tvBody;
+    ImageView tvPoster;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
+
+        tvTitle = findViewById(R.id.tvTitle);
+        tvDeck = findViewById(R.id.tvDeck);
+        tvAuthors = findViewById(R.id.tvAuthors);
+        tvPublishDate = findViewById(R.id.tvPublishDate);
+        tvPoster = findViewById(R.id.tvPoster);
+        tvBody = findViewById(R.id.tvBody);
+
+        Game game = Parcels.unwrap(getIntent().getParcelableExtra("game"));
+        tvTitle.setText(game.getTitle());
+        tvDeck.setText(game.getDeck());
+        tvAuthors.setText(game.getAuthors());
+        tvBody.setText(game.getBody());
+        tvPublishDate.setText(game.getFormattedPublishDate());
+
+        String imageUrl = game.getOriginalPosterPath();
+
+        int radius = 30; // corner radius, higher value = more rounded
+        int margin = 10; // crop margin, set to 0 for corners with no crop
+
+        Glide.with(DetailActivity.this)
+                .load(imageUrl)
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.imagenotfound)
+                .transform(new RoundedCornersTransformation(radius, margin))
+                .into(tvPoster);
     }
 }
--- app/src/main/java/com/onemanarmy/android/gamenewsapp/models/Game.java
+++ app/src/main/java/com/onemanarmy/android/gamenewsapp/models/Game.java
@@ -1,5 +1,7 @@
 package com.onemanarmy.android.gamenewsapp.models;
 
+import android.os.Build;
+import android.text.Html;
 import android.text.format.DateUtils;
 
 import org.json.JSONArray;
@@ -16,8 +18,8 @@ import java.util.TimeZone;
 @Parcel
 public class Game {
 
-    String backdropPath, posterPath, title, deck, authors, publishDate, siteDetailUrl;
-    int gameId;
+    String backdropPath, posterPath, originalPosterPath,
+            title, deck, authors, publishDate, body, siteDetailUrl;
 
     // empty constructor needed by the Parceler library
     public Game() {}
@@ -26,13 +28,24 @@ public class Game {
         JSONObject image = jsonObject.getJSONObject("image");
         backdropPath = image.getString("square_small");
         posterPath = image.getString("square_tiny");
+        originalPosterPath = image.getString("original");
 
         title = jsonObject.getString("title");
         deck = jsonObject.getString("deck");
         authors = jsonObject.getString("authors");
         publishDate = jsonObject.getString("publish_date");
+
+        body = jsonObject.getString("body");
+
+        // Convert from HTML
+        // https://stackoverflow.com/questions/2116162/how-to-display-html-in-textview
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            body = Html.fromHtml(body, Html.FROM_HTML_MODE_COMPACT).toString();
+        } else {
+            body = Html.fromHtml(body).toString();
+        }
+
         siteDetailUrl = jsonObject.getString("site_detail_url");
-        gameId = jsonObject.getInt("id");
     }
 
     public static List<Game> fromJsonArray(JSONArray gameJsonArray) throws JSONException {
@@ -51,6 +64,10 @@ public class Game {
         return backdropPath;
     }
 
+    public String getOriginalPosterPath() {
+        return originalPosterPath;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -65,7 +82,13 @@ public class Game {
         return formatDate(publishDate);
     }
 
-    public String formatDate(String date) {
+    /**
+     * Format date to from API to time spent ago.
+     * Ex: "19 hours ago"
+     * @param date String of API date
+     * @return     String of new date format
+     */
+    private String formatDate(String date) {
         // https://stackoverflow.com/questions/35858608/how-to-convert-time-to-time-ago-in-android
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // Original Format: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
@@ -80,5 +103,5 @@ public class Game {
         return date;
     }
 
-    public int getGameId() { return gameId; }
+    public String getBody() { return body; }
 }
