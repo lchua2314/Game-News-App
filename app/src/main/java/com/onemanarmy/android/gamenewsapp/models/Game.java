0e1f249fc68b765665c3711858e1172431748cce
lchua2314 <lchua2314@gmail.com>
2021-04-27 20:50:52 -0700

    Fixed timezone bug for real this time

--- app/src/main/java/com/onemanarmy/android/gamenewsapp/models/Game.java
+++ app/src/main/java/com/onemanarmy/android/gamenewsapp/models/Game.java
@@ -91,7 +91,7 @@ public class Game {
     private String formatDate(String date) {
         // https://stackoverflow.com/questions/35858608/how-to-convert-time-to-time-ago-in-android
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // Original Format: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-        sdf.setTimeZone(TimeZone.getTimeZone("PDT"));
+        // sdf.setTimeZone(TimeZone.getTimeZone("GWT")); // Commented out because it messes up the timezone +7 hours.
         try {
             long time = sdf.parse(date).getTime();
             long now = System.currentTimeMillis();
