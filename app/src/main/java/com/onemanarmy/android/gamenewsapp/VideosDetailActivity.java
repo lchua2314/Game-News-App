5e62ff2504d9bb391c81152ad2b8e2c5f8ef6c17
lchua2314 <lchua2314@gmail.com>
2021-05-27 20:31:21 -0700

    Added fullscreen mode on landscape mode on video detail activity

--- app/src/main/java/com/onemanarmy/android/gamenewsapp/VideosDetailActivity.java
+++ app/src/main/java/com/onemanarmy/android/gamenewsapp/VideosDetailActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.MediaController;
