ee1c80c4171e8d44b6814b199636356a8a6e7b4c
lchua2314 <lchua2314@gmail.com>
2021-05-30 18:04:06 -0700

    Added dropdown menu for selecting video resolution; Removed video quality button

--- app/src/main/java/com/onemanarmy/android/gamenewsapp/VideosDetailActivity.java
+++ app/src/main/java/com/onemanarmy/android/gamenewsapp/VideosDetailActivity.java
@@ -6,18 +6,19 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
 import com.onemanarmy.android.gamenewsapp.models.Videos;
-import com.onemanarmy.android.gamenewsapp.viewmodels.VideosDetailActivityViewModel;
 
 import org.parceler.Parcels;
 
@@ -27,12 +28,10 @@ public class VideosDetailActivity extends AppCompatActivity {
 
     private static final String TAG = "VideosDetailActivity";
     TextView tvTitle, tvDeck, tvPublishDate;
-    Button btnSiteDetailUrl, btnChangeVideoQuality;
+    Button btnSiteDetailUrl;
     VideoView videoView;
     ImageView ivPoster;
     MediaController mediaControls;
-    VideosDetailActivityViewModel videosDetailActivityViewModel;
-    private int videoPosition = 0;
     Videos videos;
 
     @Override
@@ -45,14 +44,11 @@ public class VideosDetailActivity extends AppCompatActivity {
         tvPublishDate = findViewById(R.id.tvPublishDate);
         ivPoster = findViewById(R.id.ivPoster);
         btnSiteDetailUrl = findViewById(R.id.btnSiteDetailUrl);
-        btnChangeVideoQuality = findViewById(R.id.btnChangeVideoQuality);
 
         videos = Parcels.unwrap(getIntent().getParcelableExtra("video"));
         tvTitle.setText(videos.getTitle());
         tvDeck.setText(videos.getDeck());
         tvPublishDate.setText(videos.getPublishDateToHumanReadable());
-
-        videosDetailActivityViewModel = new ViewModelProvider(this).get(VideosDetailActivityViewModel.class);
 
         btnSiteDetailUrl.setOnClickListener(v -> {
             Intent intent = new Intent();
@@ -60,10 +56,6 @@ public class VideosDetailActivity extends AppCompatActivity {
             intent.addCategory(Intent.CATEGORY_BROWSABLE);
             intent.setData(Uri.parse(videos.getSiteDetailUrl()));
             startActivity(intent);
-        });
-
-        btnChangeVideoQuality.setOnClickListener(v -> {
-            changeVideoQuality();
         });
 
         String imageUrl = videos.getOriginalPosterPath();
@@ -107,83 +99,90 @@ public class VideosDetailActivity extends AppCompatActivity {
         // file is ready for playback
 
         videoView.setOnPreparedListener(mediaPlayer -> {
-            // if we have a position on savedInstanceState, the video
-            // playback should start from here
-            videoView.seekTo(videoPosition);
-
             Log.i(TAG, "video is ready for playing");
 
-            if (videoPosition == 0)
-            {
-                videoView.start();
-            } else
-            {
-                // if we come from a resumed activity, video playback will
-                // be paused
-                videoView.pause();
-            }
             mediaControls.setAnchorView(videoView);
         });
+
+        videoView.start();
 
         // Hide media controls when scrolling
         View scrollView = findViewById(R.id.svDetailVideos);
         scrollView.getViewTreeObserver().addOnScrollChangedListener(() -> {
                 mediaControls.hide();
         });
+
+        // Spinner/Dropdown menu to change quality
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Low", "High", "HD"};
+
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        dropdown.setSelection(1); // default: high
+
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+
+                Uri video;
+                int progress = videoView.getCurrentPosition();
+                Log.i(TAG, "Current Position: " + progress);
+
+                switch (position) {
+                    case 0: // low
+                        video = Uri.parse(videos.getLowUrlVideo());
+                        break;
+                    case 1: // high
+                        video = Uri.parse(videos.getHighUrlVideo());
+                        break;
+                    default: // HD
+                        video = Uri.parse(videos.getHdUrlVideo());
+                }
+
+                videoView.setVideoURI(video);
+                mediaControls.hide();
+                videoView.setOnPreparedListener(mediaPlayer -> {
+                    videoView.seekTo(progress);
+                    mediaControls.setAnchorView(videoView);
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+
+            }
+        });
     }
 
     private void setVideoQuality() {
-        String quality = videosDetailActivityViewModel.getVideoQuality();
-        String btnText = "Change video quality to ";
-        Uri video;
+        Uri video = Uri.parse(videos.getHighUrlVideo());
 
+        /*
         switch (quality) {
             case "high_url":
                 video = Uri.parse(videos.getHighUrlVideo());
-                btnChangeVideoQuality.setText(btnText + "HD");
                 break;
             case "hd_url":
                 video = Uri.parse(videos.getHdUrlVideo());
-                btnChangeVideoQuality.setText(btnText + "low");
                 break;
             default: // "low_url"
                 video = Uri.parse(videos.getLowUrlVideo());
-                btnChangeVideoQuality.setText(btnText + "high");
         }
+         */
 
         videoView.setVideoURI(video);
-    }
-
-    private void changeVideoQuality() {
-        String quality = videosDetailActivityViewModel.getVideoQuality();
-        String btnText = "Change video quality to ";
-        Uri video;
-        int progress = videoView.getCurrentPosition();
-        Log.i(TAG, "Current Position: " + progress);
-
-        switch (quality) {
-            case "high_url":
-                video = Uri.parse(videos.getHdUrlVideo());
-                videosDetailActivityViewModel.setVideoQuality("hd_url");
-                btnChangeVideoQuality.setText(btnText + "low");
-                break;
-            case "hd_url":
-                video = Uri.parse(videos.getLowUrlVideo());
-                videosDetailActivityViewModel.setVideoQuality("low_url");
-                btnChangeVideoQuality.setText(btnText + "high");
-                break;
-            default: // "low_url"
-                video = Uri.parse(videos.getHighUrlVideo());
-                videosDetailActivityViewModel.setVideoQuality("high_url");
-                btnChangeVideoQuality.setText(btnText + "HD");
-        }
-
-        videoView.setVideoURI(video);
-        mediaControls.hide();
-        videoView.setOnPreparedListener(mediaPlayer -> {
-            videoView.seekTo(progress);
-            mediaControls.setAnchorView(videoView);
-        });
     }
 
     @Override
